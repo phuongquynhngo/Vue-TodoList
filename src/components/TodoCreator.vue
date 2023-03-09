f25500ea045391fb41b69f4e7620f67724985d95
Quynh <ngophuongquynh25@gmail.com>
2023-03-09 18:51:35 +0100

    Todo Validation

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,11 +1,13 @@
 <script setup>
 // import { ref, reactive} from 'vue';
-import { ref, defineEmits} from 'vue';
+import { defineEmits, reactive} from 'vue';
+//Emit Custom Events:  send data and trigger actions between components. send data from child component to parents component 
 
 const emit = defineEmits(["create-todo"])
 
 // to reactive the variable by using 1 of 2 methods: ref or reactive method
-const todo = ref("");
+// const todo = ref("");
+
 // console.log(todo.value);
 
 // const todoState = reactive({
@@ -15,17 +17,34 @@ const todo = ref("");
 
 
 
-const createTodo =()=>{
- emit("create-todo", todo.value)
+const todoState = reactive({
+  todo: "",
+  invalid: false,
+  errMsg: "",
+});
+const createTodo = () => {
+    todoState.invalid = false;
+  if (todoState.todo !== "") {
+    emit("create-todo", todoState.todo);
+    todoState.todo = "";
+    return;
+  }
+  todoState.invalid = true;
+  todoState.errMsg = "Todo value cannot be empty!";
 };
+
+
 </script>
 
 <template>
-    <div class="input-wrap">
-    <input type="text" v-model="todo">
+    <div class="input-wrap" :class="{ 'input-err': todoState.invalid }">
+    <input type="text" v-model="todoState.todo">
     <!-- <input type="text" v-model="todoState.todo"> -->
     <button @click="createTodo">Create</button>
+    
     </div>
+    <p class="err-msg" v-show="todoState.invalid">{{ todoState.errMsg }}</p>
+    <!-- error message is toggled often: using v-show, else using v-if -->
     <!-- <p>{{ todo }}</p> -->
     <!-- <p>{{ todoState.todo }}</p> -->
 </template>
@@ -38,6 +57,12 @@ const createTodo =()=>{
   display: flex;
   transition: 250ms ease;
   border: 2px solid #41b080;
+
+  &.input-err {
+    border-color: red;
+  }
+
+
   &:focus-within {
     box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
       0 -2px 4px -2px rgb(0 0 0 / 0.1);
@@ -54,5 +79,11 @@ const createTodo =()=>{
     padding: 8px 16px;
     border: none;
   }
+}
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
