1fda017d3e9cfdb1cfc41cabf3cfecffef98cf91
Quynh <ngophuongquynh25@gmail.com>
2023-03-10 16:09:19 +0100

    Display todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps } from "vue";
+import { Icon } from "@iconify/vue";
 
 // Passing Props todo from TodosView to TodoItem: declare props by using  Array syntax
 //does not giving much info on the specific prop that is defined within the component
@@ -23,10 +24,80 @@ const props = defineProps({
      to indicate whether a checkbox or radio button is checked or not. When the checked
       attribute is present on a checkbox or radio button element and its value is set to 
       "checked", the box is checked by default.  -->
-      <!-- checked attribute: here sync to value " isCompleted" of the prop "todo" 
+    <!-- checked attribute: here sync to value " isCompleted" of the prop "todo" 
       which come from todoList (check TodosView)-->
-    <input type="checkbox" :checked="todo.isCompleted"/>
+    <input type="checkbox" :checked="todo.isCompleted" />
+
+    <div class="todo">
+      <!-- <input type="text">: type into this input to edit or update todo 
+    sync tha value of this input with value of the value of its specific todo -->
+      <input v-if="todo.isEditing" type="text" :value="todo.todo" />
+      <!-- span tag: output the value of todo -->
+      <span v-else>{{ todo.todo }}</span>
+    </div>
+
+    <!-- https://icon-sets.iconify.design/ -->
+    <div class="todo-action">
+      <Icon
+        v-if="todo.isEditing"
+        icon="ph:check-circle"
+        class="icon"
+        color="#41b080"
+        width="22"
+      />
+      <Icon
+        v-else
+        icon="ph:pencil-fill"
+        class="icon"
+        color="#41b080"
+        width="22"
+      />
+      <Icon icon="ph:trash" class="icon" color="#f95e5e" width="22" />
+    </div>
   </li>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 16px 10px;
+  background-color: #f1f1f1;
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+  &:hover {
+    .todo-actions {
+      opacity: 1;
+    }
+  }
+  input[type="checkbox"] {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: #fff;
+    border-radius: 50%;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    &:checked {
+      background-color: #41b080;
+    }
+  }
+  .todo {
+    flex: 1;
+    input[type="text"] {
+      width: 100%;
+      padding: 2px 6px;
+      border: 2px solid #41b080;
+    }
+  }
+  .todo-actions {
+    display: flex;
+    gap: 6px;
+    opacity: 0;
+    transition: 150ms ease-in-out;
+    .icon {
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -3,6 +3,7 @@ import { uid } from "uid";
 import { ref } from "vue";
 import TodoCreator from "../components/TodoCreator.vue";
 import TodoItem from "../components/TodoItem.vue";
+import { Icon } from "@iconify/vue";
 
 const todoList = ref([]);
 
@@ -20,7 +21,13 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
-    <TodoItem v-for="todo in todoList" :todo="todo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
     <!-- <p>{{ todoList }}</p> -->
   </main>
 </template>
@@ -35,6 +42,20 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
