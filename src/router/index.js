92540fb6b2142c3735de173f76a996f820f5d5d8
Quynh <ngophuongquynh25@gmail.com>
2023-03-13 14:04:39 +0100

    Creating additional pages/ routes. lazy-loaded

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import TodosView from "../views/TodosView.vue";
 import NotfoundView from "../views/404View.vue";
-import AboutView from "../views/AboutView.vue";
+//import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      // default route, dont need to lazy load
       path: "/",
       name: "home",
       component: TodosView,
@@ -16,7 +17,11 @@ const router = createRouter({
     {
         path:"/about",
         name: "about",
-        component: AboutView,
+        // route level code-splitting
+        //this generates a separate chunk (About.[hash].js) which is lazy-loadded when the route is visited
+        // improve performance of the application 
+        //component: AboutView,
+        component: ()=> import("../views/AboutView.vue"),
     },
     {
       path: "/:catchall(.*)*",
